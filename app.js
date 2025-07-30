7c88fcfa49ffb3ba5d74564c4dd86e7403b708cd
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2025-07-30 20:37:31 +0530

    changes

--- app.js
+++ app.js
@@ -9,27 +9,7 @@ const fileUpload = require("express-fileupload");
 const path = require("path");
 const cors = require("cors");
 
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://ecommerce-nitin.ved.yt",
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (like mobile apps or curl)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true, // ✅ Allow cookies to be sent
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors());
 app.options("*", cors()); //for all routes
 
 // Middlewares
