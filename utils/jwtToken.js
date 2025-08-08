f3ffcdf90dbd84343205168c2b0816ad27d72431
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2025-08-08 10:50:11 +0530

    update cookie

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -13,11 +13,12 @@ const createSendToken = (user, statusCode, res) => {
 
   // options for cookie
   const cookieOptions = {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    ),
     httpOnly: true,
     secure: true,
+    expires: new Date(
+      Date.now() +
+        Number(process.env.JWT_COOKIE_EXPIRES_IN || 5) * 24 * 60 * 60 * 1000
+    ),
   };
 
   res
